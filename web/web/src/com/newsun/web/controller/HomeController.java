2b0cfbc7e1a0c0f49722251748e6582bd7ca3f65
diyaguang <diyaguang@139.com>
2017-09-05 16:00:47 +0800

    Create web mmodules at show webSite

---
+++ web/web/src/com/newsun/web/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.newsun.web.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: com.newsun.web.controller
+ * User: diyaguang
+ * Date: 2017-09-05 3:57 PM
+ */
+public class HomeController {
+}
